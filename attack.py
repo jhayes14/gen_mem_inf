75b448eaf1b4ee0dda29a960b1a51bddf4a8e72f
Jamie <j.hayes@cs.ucl.ac.uk>
2018-12-01 00:59:49 +0000

    removed dataset args

--- attack.py
+++ attack.py
@@ -17,8 +17,6 @@ import numpy as np
 from models import weights_init, Generator, Discriminator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw | fake')
-parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
